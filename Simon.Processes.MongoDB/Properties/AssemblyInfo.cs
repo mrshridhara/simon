d872cc3fb1bf3b569835c07046b30b4f9594d41f
Shridhara Rangaraju <mrshridhara@outlook.com>
2014-05-25 16:16:04 +0530

    Added process implementation for DB operations.

---
+++ Simon.Processes.MongoDB/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Simon.Processes.MongoDB")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simon")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0119c99c-14cb-4269-9693-8f0adf01926f")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
