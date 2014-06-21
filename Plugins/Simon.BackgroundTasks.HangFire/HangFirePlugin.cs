fe3c39a4325394bec3eee4c5b2dff3b9c4d27ac8
Shridhara Rangaraju <mrshridhara@outlook.com>
2014-06-21 23:01:45 +0530

    Added HangFire for background job processing.

---
+++ Plugins/Simon.BackgroundTasks.HangFire/HangFirePlugin.cs
@@ -0,0 +1,33 @@
+﻿using HangFire;
+using HangFire.Redis;
+
+namespace Simon.BackgroundTasks.HangFire
+{
+    /// <summary>
+    /// Represents the plugin intializer.
+    /// </summary>
+    public sealed class HangFirePlugin
+    {
+        /// <summary>
+        /// Initializes the plugin and updates the specified <paramref name="globalSettings"/>
+        /// if required.
+        /// </summary>
+        /// <param name="globalSettings">The global settings.</param>
+        /// <returns>
+        /// The updated instnce of <see cref="GlobalSettings"/> class.
+        /// </returns>
+        public GlobalSettings Init(GlobalSettings globalSettings)
+        {
+            if (globalSettings["HangFireRedisStorageServer"] == null)
+            {
+                globalSettings.Add("HangFireRedisStorageServer", "localhost:6379");
+            }
+
+            JobStorage.Current = new RedisStorage(globalSettings["HangFireRedisStorageServer"], 1);
+            var server = new BackgroundJobServer();
+            server.Start();
+
+            return globalSettings;
+        }
+    }
+}
