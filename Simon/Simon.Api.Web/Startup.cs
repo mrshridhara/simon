b28d8873f559766dd25e6cd615e368a9ed6a0edf
Shridhara Rangaraju <mrshridhara@outlook.com>
2014-10-11 22:23:13 +0530

    Updated angularjs package.

---
+++ Simon/Simon.Api.Web/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Owin;
+
+namespace Simon.Api.Web
+{
+    /// <summary>
+    /// Represents the OWIN start up class.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Configures the specified <paramref name="appBuilder"/>.
+        /// </summary>
+        /// <param name="appBuilder">The app builder.</param>
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.UseSimonWebApi();
+        }
+    }
+}
--- Simon/Simon.UI.Web/Properties/AssemblyInfo.cs
+++ Simon/Simon.UI.Web/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Microsoft.Owin;
 
 [assembly: AssemblyTitle("Simon.UI.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("562699ca-3161-44a0-9329-ffd618b1755a")]
+
+[assembly: OwinStartup(typeof(Simon.Api.Web.Startup))]
