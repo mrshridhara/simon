9867ac0aecd145077fe1dec7846ba53b928d236a
Shridhara Rangaraju <mrshridhara@outlook.com>
2015-02-20 22:02:10 +0530

    Initial commit of application manipulation.

---
+++ Core/Simon.UI.Web/App/02_Applications/01_Services/01_Applications.Service.js
@@ -0,0 +1,17 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular
+        .module('Applications')
+        .service('ApplicationsService', applicationsService);
+
+    applicationsService.$inject = [
+        '$http',
+        '$location',
+        'BreadcrumbService'
+    ];
+
+    function applicationsService($http, $location, breadcrumbService) {
+        /* jshint validthis: true */
+    }
+}(angular));
---
+++ Core/Simon.UI.Web/App/02_Applications/02_Controllers/03_EditApplication.Controller.js
@@ -0,0 +1,20 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular
+        .module('Applications')
+        .controller('EditApplication', editApplication);
+
+    editApplication.$inject = [
+        '$location',
+        '$routeParams',
+        'PageHeadService',
+        'NavbarService',
+        'BreadcrumbService',
+        'ApplicationsService'
+    ];
+
+    function editApplication($location, $routeParams, pageHeadService, navbarService, breadcrumbService, applicationsService) {
+        /* jshint validthis: true */
+    }
+}(angular));
